e3572320fc4a9573c1194c14c42b524d425cdc29
math <math@maths-Mac-Studio.local>
2025-09-10 09:51:04 +0800

    first commit

---
+++ doc/build.sh
@@ -0,0 +1,12 @@
+mainCFG="mysite.conf"
+otherCFG="mysite.conf"
+
+
+python jemdoc -c ${mainCFG}  -o ../docs/   index.jemdoc
+#python jemdoc -c ${otherCFG} -o ../docs/   publications.jemdoc
+#python jemdoc -c ${otherCFG} -o ../docs/   news.jemdoc
+#python jemdoc -c ${otherCFG} -o ../docs/   joining.jemdoc
+#python jemdoc -c ${otherCFG} -o ../docs/   ethic.jemdoc
+#python jemdoc -c ${otherCFG} -o ../docs/   blog.jemdoc
+#python jemdoc -c ${otherCFG} -o ../docs/   inv_fair.jemdoc
+#python jemdoc -c ${otherCFG} -o ../docs/   note.jemdoc
