12d9a8d7f758322cdd83f3140ac97a9b89e52d98
math <math@maths-Mac-Studio.local>
2025-09-11 09:41:29 +0800

    first build

--- doc/build.sh
+++ doc/build.sh
@@ -2,7 +2,7 @@ mainCFG="mysite.conf"
 otherCFG="mysite.conf"
 
 
-python jemdoc -c ${mainCFG}  -o ../docs/   index.jemdoc
+python3 jemdoc -c ${mainCFG}  -o ../docs/   index.jemdoc
 #python jemdoc -c ${otherCFG} -o ../docs/   publications.jemdoc
 #python jemdoc -c ${otherCFG} -o ../docs/   news.jemdoc
 #python jemdoc -c ${otherCFG} -o ../docs/   joining.jemdoc
